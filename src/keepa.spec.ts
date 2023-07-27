25cf7660d9328bbaa119a800af942fe60dae975a
Hung Tran <tmhung88@outlook.com>
2023-07-27 19:59:16 +0700

    Add keepa decode

---
+++ src/keepa.spec.ts
@@ -0,0 +1,18 @@
+import fs from 'fs'
+import * as fflate from 'fflate'
+
+describe('keepa', () => {
+  it('decode product details from browser cache', () => {
+    const cacheContent = fs.readFileSync('./data/keepaCache_exported_data.json')
+    // you can see asins in Developer Tools => Application => IndexedDB => keepaCache => cache
+    // choose asin of interest
+    const asinIndex = 0
+    const keepaCache = JSON.parse(cacheContent.toString())
+    const asinUnit8Array = Uint8Array.from(Object.values(keepaCache.cache[asinIndex]))
+    const asinContent = fflate.strFromU8(fflate.inflateSync(asinUnit8Array))
+    // deserialize the content to get a Product object
+    // https://keepa.com/#!discuss/t/product-object/116
+    const asin = JSON.parse(asinContent)
+    expect(asin.title).toStrictEqual('Ultimate Artisan Banneton Bread Proofing Basket Set of 2 - Round & Oval Rattan Proofing Baskets, Dough Scraper, Recipe Book - Sourdough Bread Baking Supplies - Perfect Bread Making Tools and Supplies')
+  })
+})
