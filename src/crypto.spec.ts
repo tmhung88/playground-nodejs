4e7c776260d9a9a01a84854fc0bb94c8e905f2ce
Hung Tran <tmhung88@outlook.com>
2023-08-13 16:09:58 +0700

    Test crypto md5

---
+++ src/crypto.spec.ts
@@ -0,0 +1,11 @@
+import * as crypto from 'crypto'
+import { range } from 'lodash'
+describe('crypto', () => {
+  describe('md5', () => {
+    it('should provide the same hash string', () => {
+      const hash = crypto.createHash('md5').update('hello world').digest('hex')
+      expect(hash).toStrictEqual('5eb63bbbe01eeed093cb22bb8f5acdc3')
+      console.log(range(0, 10).map(i => i))
+    })
+  })
+})
